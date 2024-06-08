6392be8436babecc0b0331a8185e7c7d02271464
wwwform <168577329+wwwform@users.noreply.github.com>
2024-06-07 22:50:48 -0300

    Atualizar o js

---
+++ js/versiculo.js
@@ -0,0 +1,22 @@
+import versiculos from './versiculos.json';
+
+export function initVersiculo() {
+  const btnGerarVersiculo = document.getElementById('gerar-versiculo');
+  const versiculoTextarea = document.getElementById('versiculo');
+  const btnCompartilhar = document.getElementById('enviar-whatsapp');
+
+  btnGerarVersiculo.addEventListener('click', () => {
+    const versiculoAleatorio = gerarVersiculoAleatorio();
+    versiculoTextarea.value = versiculoAleatorio;
+    atualizarLinkWhatsApp(versiculoAleatorio);
+  });
+
+  function gerarVersiculoAleatorio() {
+    const indiceAleatorio = Math.floor(Math.random() * versiculos.length);
+    return versiculos[indiceAleatorio];
+  }
+
+  function atualizarLinkWhatsApp(versiculo) {
+    btnCompartilhar.href = `https://wa.me/?text=${encodeURIComponent(versiculo)}`;
+  }
+}
