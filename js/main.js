821c83fdf390f89bee2c3c5eee668324335f0726
wwwform <168577329+wwwform@users.noreply.github.com>
2024-06-08 09:37:10 -0300

    Atualizar o main.js

--- js/main.js
+++ js/main.js
@@ -1,11 +1,53 @@
-import { initVersiculo } from './versiculo.js';
-import { initMenu } from './menu.js';
+import versiculos from './versiculos.json';
 
 document.addEventListener('DOMContentLoaded', () => {
   initVersiculo();
   initMenu();
   initBiblia();
 });
+
+function initVersiculo() {
+  const btnGerarVersiculo = document.getElementById('gerar-versiculo');
+  const versiculoTextarea = document.getElementById('versiculo');
+  const btnCompartilhar = document.getElementById('enviar-whatsapp');
+
+  btnGerarVersiculo.addEventListener('click', () => {
+    const versiculoAleatorio = gerarVersiculoAleatorio();
+    versiculoTextarea.value = versiculoAleatorio;
+    atualizarLinkWhatsApp(versiculoAleatorio);
+  });
+
+  function gerarVersiculoAleatorio() {
+    const indiceAleatorio = Math.floor(Math.random() * versiculos.length);
+    return versiculos[indiceAleatorio];
+  }
+
+  function atualizarLinkWhatsApp(versiculo) {
+    btnCompartilhar.href = `https://wa.me/?text=${encodeURIComponent(versiculo)}`;
+  }
+}
+
+function initMenu() {
+  const menuToggle = document.getElementById('menu-toggle');
+  const menu = document.getElementById('menu');
+  const menuLinks = document.querySelectorAll('.menu-link');
+
+  menuToggle.addEventListener('click', () => {
+    menu.classList.toggle('showing');
+  });
+
+  menuLinks.forEach(link => {
+    link.addEventListener('click', (event) => {
+      event.preventDefault();
+      const targetId = link.getAttribute('data-target');
+      document.querySelectorAll('.content').forEach(section => {
+        section.classList.remove('active');
+      });
+      document.getElementById(targetId).classList.add('active');
+      menu.classList.remove('showing');
+    });
+  });
+}
 
 function initBiblia() {
   fetch('https://raw.githubusercontent.com/thiagobodruk/biblia/master/xml/nvi.min.xml')
@@ -31,12 +73,16 @@ function initBiblia() {
         bookItem.appendChild(bookLink);
         bookList.appendChild(bookItem);
       }
+    })
+    .catch(error => {
+      console.error('Erro ao carregar a Bíblia:', error);
+      // Exibir mensagem de erro para o usuário (opcional)
     });
 }
 
 function loadChapters(book, chapterList, verseList) {
-  chapterList.innerHTML = ''; // Limpar capítulos anteriores
-  verseList.innerHTML = '';   // Limpar versículos anteriores
+  chapterList.innerHTML = '';
+  verseList.innerHTML = '';
 
   const chapters = book.getElementsByTagName('c');
   for (let i = 0; i < chapters.length; i++) {
@@ -53,7 +99,7 @@ function loadChapters(book, chapterList, verseList) {
 }
 
 function loadVerses(chapter, verseList) {
-  verseList.innerHTML = ''; // Limpar versículos anteriores
+  verseList.innerHTML = '';
 
   const verses = chapter.getElementsByTagName('v');
   for (let i = 0; i < verses.length; i++) {
