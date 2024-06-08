2cf02dbd0cac52b9221dd204b487af76565f57eb
wwwform <168577329+wwwform@users.noreply.github.com>
2024-06-08 14:54:29 -0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,104 +1,106 @@
 import { initVersiculo } from './versiculo.js';
 
 document.addEventListener('DOMContentLoaded', () => {
-    initVersiculo();
-    initMenu();
-    initBiblia();
-
-    // Mostrar a seção inicial (Home) por padrão
-    showSection('home');
+  initVersiculo();
+  initMenu();
+  initBiblia();
+  showSection('home'); // Exibe a seção "Home" inicialmente
 });
 
-// Função para mostrar a seção selecionada
 function showSection(sectionId) {
-    const sections = document.querySelectorAll('.content');
-    sections.forEach(section => {
-        section.classList.remove('active');
-    });
-    document.getElementById(sectionId).classList.add('active');
+  const sections = document.querySelectorAll('.content');
+  sections.forEach(section => {
+    section.style.display = 'none'; // Esconde todas as seções
+  });
+  document.getElementById(sectionId).style.display = 'block'; // Exibe a seção selecionada
 
-    // Remover classe "active" dos links do menu
-    const menuLinks = document.querySelectorAll('#menu a');
-    menuLinks.forEach(link => {
-        link.classList.remove('active');
-    });
+  // Remove a classe "active" de todos os links do menu
+  const menuLinks = document.querySelectorAll('nav a');
+  menuLinks.forEach(link => {
+    link.classList.remove('active');
+  });
 
-    // Adicionar classe "active" ao link correspondente à seção
-    const activeLink = document.querySelector(`#menu a[href="#${sectionId}"]`);
-    if (activeLink) {
-        activeLink.classList.add('active');
-    }
+  // Adiciona a classe "active" ao link da seção selecionada
+  event.target.classList.add('active');
 }
 
-// Função para mostrar/esconder o menu em telas menores
 function initMenu() {
-    const menuToggle = document.getElementById('menu-toggle');
-    const menu = document.getElementById('menu');
+  const menuToggle = document.getElementById('menu-toggle');
+  const menu = document.getElementById('menu');
 
-    menuToggle.addEventListener('click', () => {
-        menu.classList.toggle('show');
+  menuToggle.addEventListener('click', () => {
+    menu.classList.toggle('show'); // Alterna a classe "show" para mostrar/esconder o menu
+  });
+
+  // Adiciona um ouvinte de eventos para cada link do menu
+  const menuLinks = document.querySelectorAll('nav a');
+  menuLinks.forEach(link => {
+    link.addEventListener('click', (event) => {
+      event.preventDefault(); // Impede o comportamento padrão do link
+      const sectionId = link.getAttribute('href').substring(1); // Obtém o ID da seção do atributo href
+      showSection(sectionId); // Chama a função para mostrar a seção correspondente
     });
+  });
 }
 
-// Função para carregar e exibir a Bíblia
 function initBiblia() {
-    fetch('https://raw.githubusercontent.com/thiagobodruk/biblia/master/xml/nvi.min.xml')
-        .then(response => response.text())
-        .then(data => {
-            const parser = new DOMParser();
-            const xmlDoc = parser.parseFromString(data, 'application/xml');
-            const books = xmlDoc.getElementsByTagName('b');
-            const bookList = document.getElementById('book-list');
-            const chapterList = document.getElementById('chapter-list');
-            const verseList = document.getElementById('verse-list');
+  fetch('https://raw.githubusercontent.com/thiagobodruk/biblia/master/xml/nvi.min.xml')
+    .then(response => response.text())
+    .then(data => {
+      const parser = new DOMParser();
+      const xmlDoc = parser.parseFromString(data, 'application/xml');
+      const books = xmlDoc.getElementsByTagName('b');
+      const bookList = document.getElementById('book-list');
+      const chapterList = document.getElementById('chapter-list');
+      const verseList = document.getElementById('verse-list');
 
-            // Carregar livros
-            for (let i = 0; i < books.length; i++) {
-                const book = books[i];
-                const bookName = book.getAttribute('name');
-                const bookItem = document.createElement('li');
-                const bookLink = document.createElement('a');
-                bookLink.href = '#';
-                bookLink.textContent = bookName;
-                bookLink.addEventListener('click', () => loadChapters(book, chapterList, verseList));
-                bookItem.appendChild(bookLink);
-                bookList.appendChild(bookItem);
-            }
-        })
-        .catch(error => {
-            console.error('Erro ao carregar a Bíblia:', error);
-            verseList.innerHTML = '<p>Erro ao carregar a Bíblia. Por favor, tente novamente mais tarde.</p>';
-        });
+      // Carregar livros
+      for (let i = 0; i < books.length; i++) {
+        const book = books[i];
+        const bookName = book.getAttribute('name');
+        const bookItem = document.createElement('li');
+        const bookLink = document.createElement('a');
+        bookLink.href = '#';
+        bookLink.textContent = bookName;
+        bookLink.addEventListener('click', () => loadChapters(book, chapterList, verseList));
+        bookItem.appendChild(bookLink);
+        bookList.appendChild(bookItem);
+      }
+    })
+    .catch(error => {
+      console.error('Erro ao carregar a Bíblia:', error);
+      verseList.innerHTML = '<p>Erro ao carregar a Bíblia. Por favor, tente novamente mais tarde.</p>';
+    });
 }
 
 function loadChapters(book, chapterList, verseList) {
-    chapterList.innerHTML = '';
-    verseList.innerHTML = '';
+  chapterList.innerHTML = '';
+  verseList.innerHTML = '';
 
-    const chapters = book.getElementsByTagName('c');
-    for (let i = 0; i < chapters.length; i++) {
-        const chapter = chapters[i];
-        const chapterNumber = chapter.getAttribute('n');
-        const chapterItem = document.createElement('li');
-        const chapterLink = document.createElement('a');
-        chapterLink.href = '#';
-        chapterLink.textContent = `Capítulo ${chapterNumber}`;
-        chapterLink.addEventListener('click', () => loadVerses(chapter, verseList));
-        chapterItem.appendChild(chapterLink);
-        chapterList.appendChild(chapterItem);
-    }
+  const chapters = book.getElementsByTagName('c');
+  for (let i = 0; i < chapters.length; i++) {
+    const chapter = chapters[i];
+    const chapterNumber = chapter.getAttribute('n');
+    const chapterItem = document.createElement('li');
+    const chapterLink = document.createElement('a');
+    chapterLink.href = '#';
+    chapterLink.textContent = `Capítulo ${chapterNumber}`;
+    chapterLink.addEventListener('click', () => loadVerses(chapter, verseList));
+    chapterItem.appendChild(chapterLink);
+    chapterList.appendChild(chapterItem);
+  }
 }
 
 function loadVerses(chapter, verseList) {
-    verseList.innerHTML = '';
+  verseList.innerHTML = '';
 
-    const verses = chapter.getElementsByTagName('v');
-    for (let i = 0; i < verses.length; i++) {
-        const verse = verses[i];
-        const verseNumber = verse.getAttribute('n');
-        const verseText = verse.textContent;
-        const verseItem = document.createElement('p');
-        verseItem.textContent = `${verseNumber}: ${verseText}`;
-        verseList.appendChild(verseItem);
-    }
+  const verses = chapter.getElementsByTagName('v');
+  for (let i = 0; i < verses.length; i++) {
+    const verse = verses[i];
+    const verseNumber = verse.getAttribute('n');
+    const verseText = verse.textContent;
+    const verseItem = document.createElement('p');
+    verseItem.textContent = `${verseNumber}: ${verseText}`;
+    verseList.appendChild(verseItem);
+  }
 }
