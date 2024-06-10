5bc301d70e82c2960b7eded126dfc3099a6f0b92
wwwform <168577329+wwwform@users.noreply.github.com>
2024-06-10 10:20:29 -0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,13 +1,17 @@
 import { initMenu } from './menu.js';
-import { initVersiculo } from './versiculo.js'; // Corrigir o caminho se necessário
+import { initVersiculo } from './versiculo.js'; // Certifique-se de que o caminho está correto
 
 document.addEventListener('DOMContentLoaded', () => {
-  console.log('DOMContentLoaded');
+  console.log('DOMContentLoaded event triggered');
   initMenu();
+  console.log('initMenu called');
   initVersiculo();
+  console.log('initVersiculo called');
   initBiblia();
+  console.log('initBiblia called');
   showSection('home'); 
   document.getElementById('home-link').classList.add('active'); 
+  console.log('Home section shown and home link activated');
 });
 
 function showSection(sectionId) {
@@ -15,6 +19,7 @@ function showSection(sectionId) {
   sections.forEach(section => {
     section.style.display = 'none';
   });
+  console.log(`Section ${sectionId} will be shown`);
 
   document.getElementById(sectionId).style.display = 'block';
 
@@ -24,11 +29,13 @@ function showSection(sectionId) {
   });
 
   document.getElementById(sectionId + '-link').classList.add('active'); 
+  console.log(`${sectionId} link activated`);
 }
 
 async function initBiblia() {
   try {
-    const response = await fetch('js/nvi.xml'); // Carrega o XML localmente
+    console.log('Fetching nvi.xml');
+    const response = await fetch('biblia.xml'); // Carrega o XML localmente
     const data = await response.text();
     const parser = new DOMParser();
     const xmlDoc = parser.parseFromString(data, 'application/xml');
@@ -48,6 +55,7 @@ async function initBiblia() {
       bookItem.appendChild(bookLink);
       bookList.appendChild(bookItem);
     }
+    console.log('Bible books loaded successfully');
   } catch (error) {
     console.error('Erro ao carregar a Bíblia:', error);
     const verseList = document.getElementById('verse-list');
@@ -56,6 +64,7 @@ async function initBiblia() {
 }
 
 function loadChapters(book, chapterList, verseList) {
+  console.log(`Loading chapters for book ${book.getAttribute('name')}`);
   chapterList.innerHTML = '';
   verseList.innerHTML = '';
 
@@ -71,9 +80,11 @@ function loadChapters(book, chapterList, verseList) {
     chapterItem.appendChild(chapterLink);
     chapterList.appendChild(chapterItem);
   }
+  console.log('Chapters loaded successfully');
 }
 
 function loadVerses(chapter, verseList) {
+  console.log(`Loading verses for chapter ${chapter.getAttribute('n')}`);
   verseList.innerHTML = '';
 
   const verses = chapter.getElementsByTagName('v');
@@ -85,4 +96,5 @@ function loadVerses(chapter, verseList) {
     verseItem.textContent = `${verseNumber}: ${verseText}`;
     verseList.appendChild(verseItem);
   }
+  console.log('Verses loaded successfully');
 }
